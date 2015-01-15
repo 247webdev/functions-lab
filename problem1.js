a8e7abbbe382d29aa1d0824b2d20951f084d12a0
Christian Chandler <247webdev+github@googlemail.com>
2015-01-15 11:29:41 -0800

    adding check for two equal numbers

--- problem1.js
+++ problem1.js
@@ -1,24 +1,16 @@
 //Functions lab WDI-16 Day 3 HW for Christian Chandler
-//*** see below for alternate solution if not using if-then-else construct ***
 
 // Problem #1
 function maxOfTwoNumbers(a, b){
     if(a > b){
         return a;
-    } else {
+    } else if (a < b) {
         return b;
+    } else {
+    	return "The two numbers are equal.";
     }
 }
 
 maxOfTwoNumbers(7, 1); // result is 7
 maxOfTwoNumbers(4, 9); // result is 9
-
-/*         The following is an alternate syntax to using if-then-else construct...
-
-function maxOfTwoNumbersAlternateSyntax(a, b){
-    return (a > b ? a : b); 
-}
-
-maxOfTwoNumbersAlternateSyntax(6, 3);  // result is 6
-
- */
+maxOfTwoNumbers(5, 5); // result is 9
