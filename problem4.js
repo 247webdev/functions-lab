bca313bf084bc56c7eb5852729d41afcbe386a17
Christian Chandler <247webdev+github@googlemail.com>
2015-01-14 23:33:23 -0800

    first commit for problem4

---
+++ problem4.js
@@ -0,0 +1,24 @@
+// Functions lab WDI-16 Day 3 HW for Christain Chandler
+
+// Problem #4
+function sumArray(passedArray) {
+    for(var i = 0, runningTot = 0; i < passedArray.length; i++){
+        runningTot = runningTot + passedArray[i];
+    }
+    return runningTot;
+}
+
+function multiplyArray(passedArray) {
+    for(var i = 0, runningTot = 1; i < passedArray.length; i++){
+        runningTot = runningTot * passedArray[i];
+    }
+    return runningTot;
+}
+
+var testArray1 = [1,2,3,4,5];
+var testArray2 = [5,4,3,2,1];
+var testArray3 = [10,20,30,40,50];
+var testArray4 = [7,5,9,3,-2];
+
+sumArray(testArray1); // returns 15
+multiplyArray(testArray4); // returns -1890
